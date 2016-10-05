9be8aa054240b548751a1d3a9a1d80d9cf121ee9
Alexander Kuvaev <alexander@kuvaev.me>
2016-10-05 15:06:30 +0300

    little changes

--- src/level.rs
+++ src/level.rs
@@ -14,14 +14,14 @@ impl Level {
     }
 
     pub fn generate_mesh(&self) -> (Vec<Vertex>, Vec<u32>) {
-        let mut vertex_data = Vec::<Vertex>::new();
-        vertex_data.push(Vertex { pos: [ -0.5,  0.0 ], color: [1.0, 1.0, 0.0] });
-        vertex_data.push(Vertex { pos: [  0.5,  0.8 ], color: [1.0, 0.0, 0.0] });
-        vertex_data.push(Vertex { pos: [ -0.3, -0.1 ], color: [1.0, 1.0, 0.0] });
-        vertex_data.push(Vertex { pos: [  0.4, -0.5 ], color: [0.0, 1.0, 1.0] });
-        vertex_data.push(Vertex { pos: [  0.5,  0.4 ], color: [0.0, 1.0, 0.0] });
-        vertex_data.push(Vertex { pos: [  0.9, -0.2 ], color: [1.0, 0.0, 1.0] });
-        let mut index_data = vec![0, 1, 2, 3, 4, 5];
+        let vertex_data = vec![
+            Vertex { pos: [ -0.5,  0.0 ], color: [1.0, 1.0, 0.0] },
+            Vertex { pos: [  0.5,  0.8 ], color: [1.0, 0.0, 0.0] },
+            Vertex { pos: [ -0.3, -0.1 ], color: [1.0, 1.0, 0.0] },
+            Vertex { pos: [  0.4, -0.5 ], color: [0.0, 1.0, 1.0] },
+            Vertex { pos: [  0.5,  0.4 ], color: [0.0, 1.0, 0.0] },
+            Vertex { pos: [  0.9, -0.2 ], color: [1.0, 0.0, 1.0] }];
+        let index_data = vec![0, 1, 2, 3, 4, 5];
         (vertex_data, index_data)
     }
 }
